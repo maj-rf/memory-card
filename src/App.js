1c8689e2b18f7dcd499c0dc7e9a951386fca43ea
bananabread08 <rmkfermin@gmail.com>
2022-06-23 22:38:59 +0800

    implement HighScore, currentScore and gameOver checker

--- src/App.js
+++ src/App.js
@@ -4,30 +4,44 @@ import Gameboard from './components/Gameboard/Gameboard';
 import { shuffleDeck } from './utils/shuffleDeck';
 import { useQuery } from 'react-query';
 import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
   const { isLoading, error, data } = useQuery('mortys', () =>
     axios('https://rickandmortyapi.com/api/character/?name=morty&status=alive')
   );
   const [deck, setDeck] = useState([]);
+  const [picks, setPicks] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  useEffect(() => {
+    setDeck(data?.data.results);
+  }, [data]);
 
-  const playRound = () => {
-    setDeck(shuffleDeck(data?.data.results));
+  const playRound = (id) => {
+    setDeck((prevState) => shuffleDeck(prevState));
+    if (picks.includes(id)) return gameOver();
+    setPicks(picks.concat(id));
+    setScore((prevState) => prevState + 1);
+  };
+
+  const gameOver = () => {
+    setScore(0);
+    setPicks([]);
+    if (score > highScore) setHighScore(score);
+    return alert('gameover');
   };
 
   if (error) return <h1>{error.message}</h1>;
 
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem optio quam
-        vero cumque.
-      </p>
+      <h2>Score: {score}</h2>
+      <h2>HighScore: {highScore}</h2>
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <Gameboard data={data?.data.results} playRound={playRound} />
+        <Gameboard data={deck} playRound={playRound} />
       )}
     </div>
   );
