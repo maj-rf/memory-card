22abb57f5b81ccbe217f535ff00a45672be91fa1
bananabread08 <rmkfermin@gmail.com>
2022-06-24 11:08:46 +0800

    update Card and Header styles (borders, width, font-size)

--- src/components/Card/Card.style.js
+++ src/components/Card/Card.style.js
@@ -2,17 +2,18 @@ import styled from 'styled-components';
 
 export const CardWrapper = styled.div`
   cursor: pointer;
-  border: 0.5rem solid papayawhip;
-  border-radius: 1rem;
-  width: fit-content;
-
+  border: 0.5rem solid;
+  border-image: linear-gradient(to right, #3f5efb, #fc466b) 1;
+  border-radius: 16px;
   img {
     height: 200px;
+    display: block;
   }
 
   p {
     text-align: center;
     padding: 0;
     margin: 0;
+    background-color: #fff;
   }
 `;
