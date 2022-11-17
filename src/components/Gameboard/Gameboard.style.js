b059731a7e196e3383ccb3491eeab83f908229e6
bananabread08 <rmkfermin@gmail.com>
2022-11-17 13:22:03 +0800

    initial commit
    install tailwind, eslint, prettier

--- src/components/Gameboard/Gameboard.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const GameboardWrapper = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 3rem;
-`;
