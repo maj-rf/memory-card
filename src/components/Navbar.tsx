d58b2411321ea316b43cbea81fe80cea47f17f69
bananabread08 <rmkfermin@gmail.com>
2022-11-18 09:48:06 +0800

    fix nav spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ type Props = {
 export const Navbar = (props: Props) => {
   return (
     <nav className="border-gray-200 border-b-2 px-5 py-2.5">
-      <div className="container flex flex-wrap justify-between items-center">
+      <div className="flex flex-wrap justify-between items-center gap-2">
         <h1>Memory Game</h1>
         <div className=" flex gap-4">
           <p className=" bg-zinc-700 py-2 px-3 rounded-md">
